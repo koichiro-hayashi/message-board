19d23895c27d4ab4a001cb50493c98c1a60a232c
koichiro_hayashi <koroichi0427@gmail.com>
2016-02-11 10:02:24 +0000

    create message model

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160211065458) do
+ActiveRecord::Schema.define(version: 20160211095114) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "name"
-    t.string   "bode"
+    t.string   "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
